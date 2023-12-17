6f753739f46b421a1ad850d2691de0a778972763
Xia Wangjun <103339777+030525@users.noreply.github.com>
2023-12-17 19:38:14 +0800

    download docker

--- config/config.sh
+++ config/config.sh
@@ -6,4 +6,8 @@ apt install -y vim ufw net-tools inetutils-ping
 
 systemctl enable ssh
 
-echo 'install docker not use snap,it causes problem'
+ curl -o docker.deb https://desktop.docker.com/linux/main/amd64/docker-desktop-4.26.1-amd64.deb?utm_source=docker&utm_medium=webreferral&utm_campaign=docs-driven-download-linux-amd64
+
+ dpkg -i docker.deb
+
+ rm docker.deb
