63aaaadb8ade8db8919bf182543f2ca193eb4060
Xia Wangjun <103339777+030525@users.noreply.github.com>
2023-12-17 20:09:48 +0800

    Update config.sh

--- config/config.sh
+++ config/config.sh
@@ -6,8 +6,4 @@ apt install -y vim ufw net-tools inetutils-ping
 
 systemctl enable ssh
 
- curl -o docker.deb https://desktop.docker.com/linux/main/amd64/docker-desktop-4.26.1-amd64.deb?utm_source=docker&utm_medium=webreferral&utm_campaign=docs-driven-download-linux-amd64
-
- dpkg -i docker.deb
-
- rm docker.deb
+echo 'install docker not from snap or apt'
